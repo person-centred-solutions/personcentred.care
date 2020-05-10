9ae3be4718af46f33b07efa054f8823c896a41f6
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-05-10 18:13:00 +0000

    feat: fetch care counts

---
+++ functions/carecounts/CareCounts.go
@@ -0,0 +1,7 @@
+package carecounts
+
+// CareCounts contain count of publications on different care types
+type CareCounts struct {
+	PersonCentredCarePublications  uint32
+	PatientCentredCarePublications uint32
+}
---
+++ functions/carecounts/FetchCareCounts.go
@@ -0,0 +1,38 @@
+package carecounts
+
+import (
+	"functions/pubmed"
+	"log"
+	"strings"
+)
+
+// FetchCareCounts fetches counts of publications on person and patient centred cares
+func FetchCareCounts() (CareCounts, err) {
+	personCentredTerms := []string{"person-centred", "person-centered", "person centred", "person centered"}
+
+	patientCentredTerms := []string{"patient-centred", "patient-centered", "patient centred", "patient centered"}
+
+	var output CareCounts
+
+	log.Printf("%v", constructSearchTerm(personCentredTerms))
+	return output, nil
+}
+
+func fetchPubmedCount(terms string) (uint32, error) {
+	pubmedResponse, err := pubmed.FetchFromPubmed(terms)
+	if err != nil {
+		return 0, err
+	}
+	return pubmedResponse.Count, nil
+}
+
+func constructSearchTerm(alternativeTerms []string) string {
+	var termsTransformed []string
+
+	for _, term := range alternativeTerms {
+		termTransformed = "\"" + term + "\"[All Terms]"
+		termsUnion = append(termsUnion, termTransformed)
+	}
+
+	return strings.Join(termsTransformed, " OR ")
+}
