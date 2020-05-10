754a1391b08d19be1da5243873ad751a03213558
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-05-10 16:02:22 +0000

    fix: proper entrez response

--- functions/entrez/Types.go
+++ functions/entrez/Types.go
@@ -7,7 +7,9 @@ type EntrezResponse struct {
 	Count    uint32   `xml:"Count" json:"Count"`
 	RetMax   uint32   `xml:"RetMax" json:"RetMax"`
 	RetStart uint32   `xml:"RetStart" json:"RetStart"`
-	IdList   []IdItem `xml:"IdList" json:"ids"`
+	IdList   IdList   `xml:"IdList" json:"ids"`
 }
 
-type IdItem uint32
+type IdList struct {
+	Items []uint32 `xml:"Id" json:"list"`
+}
