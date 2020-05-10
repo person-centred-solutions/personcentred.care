601bebc081818a39b83ea2d1ecc7b868272b8eeb
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-05-10 17:47:17 +0000

    fix: proper pubmed fetch

--- functions/go.mod
+++ functions/go.mod
@@ -2,4 +2,4 @@ module functions
 
 go 1.14
 
-require github.com/imroc/req v0.3.0 // indirect
+require github.com/imroc/req v0.3.0
--- functions/pubmed/FetchFromPubmed.go
+++ functions/pubmed/FetchFromPubmed.go
@@ -9,15 +9,18 @@ import (
 const ncbiEntrezAPIURL = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
 const pubmedDatabaseName = "pubmed"
 
+// FetchFromPubmed performs article search in pubmed database
 func FetchFromPubmed(searchTerms string) (entrez.EntrezResponse, error) {
 	params := req.Param{
 		"db":   pubmedDatabaseName,
 		"term": searchTerms,
 	}
+	response, err := req.Get(ncbiEntrezAPIURL, params)
+
 	var pubmedResponse entrez.EntrezResponse
-	_, err := req.Get(ncbiEntrezAPIURL, params, req.BodyXML(&pubmedResponse))
+	response.ToXML(&pubmedResponse)
 	if err != nil {
-		return "", err
+		return entrez.EntrezResponse{}, err
 	}
 	return pubmedResponse, nil
 }
