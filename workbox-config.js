0a901650bcab7a97af7e981ae3c22f0166a4c989
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-05-19 15:47:09 +0000

    feat: service worker

---
+++ workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "public/_site",
+  "globPatterns": [
+    "**/*.{html,jpg,png,svg,xml,ico,webmanifest,css,gif,webm,json,txt,js}"
+  ],
+  "swDest": "public/_site/sw.js"
+};
